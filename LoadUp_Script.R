a8fb26e200a7cbaf117d146b281e0a826295c49e
Abhinav R. Joshi <pbcd.brrb@gmail.com>
2017-03-13 13:58:26 -0700

    Adding Loadup script

---
+++ LoadUp_Script.R
@@ -0,0 +1,24 @@
+library(dplyr)
+library(lubridate)
+library(ggplot2)
+library(tidyr)
+setwd("~/Documents/DS Project")
+nyc <- read.csv("NYPD_Motor_Vehicle_Cleaned.csv" , stringsAsFactors = FALSE)
+
+# Remove X column:
+nyc <- nyc %>%
+  select(-X)
+
+
+# Convert to date
+nyc$DATE2 <- mdy(nyc$DATE)
+
+
+# Separate Time and Date columns
+nyc <- separate(nyc, TIME , c("Hour", "Min"), sep = ":")
+nyc <- separate(nyc, DATE2 , c("YEAR", "MONTH" , "DAY"), sep = "-")
+
+
+# Removing 2012 and 2017 observations
+nyc <- nyc %>%
+  filter(YEAR != '2012' , YEAR != '2017')
